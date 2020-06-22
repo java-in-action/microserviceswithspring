9d258ec4a61454b929212cf9269508801d6c5e08
Eric <e.alfonso@accenture.com>
2020-06-22 15:54:51 -0500

    Changes regarding session two about microservices

--- cargo-module-service/src/main/java/com/microservices/cargomoduleservice/Configuration.java
+++ cargo-module-service/src/main/java/com/microservices/cargomoduleservice/Configuration.java
@@ -1,38 +1,27 @@
-package com.microservices.cargomoduleservice.bean;
+package com.microservices.cargomoduleservice;
 
-public class Cargo {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties("cargo-module-service")
+@Component
+public class Configuration {
 
 	private int priceOfDelivery;
 	private int priceOfShipping;
 	
-	
-	
 	public int getPriceOfDelivery() {
 		return priceOfDelivery;
 	}
-
-
-
 	public void setPriceOfDelivery(int priceOfDelivery) {
 		this.priceOfDelivery = priceOfDelivery;
 	}
-
-
-
 	public int getPriceOfShipping() {
 		return priceOfShipping;
 	}
-
-
-
 	public void setPriceOfShipping(int priceOfShipping) {
 		this.priceOfShipping = priceOfShipping;
 	}
-
-
-
-	public Cargo(int priceOfDelivery, int priceOfShipping) {
-		this.priceOfDelivery = priceOfDelivery;
-		this.priceOfShipping = priceOfShipping;
-	}
+	
+	
 }
--- cargo-module-service/src/main/java/com/microservices/cargomoduleservice/controller/CargoController.java
+++ cargo-module-service/src/main/java/com/microservices/cargomoduleservice/controller/CargoController.java
@@ -1,15 +1,22 @@
 package com.microservices.cargomoduleservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservices.cargomoduleservice.Configuration;
+import com.microservices.cargomoduleservice.bean.Cargo;
+
 @RestController
 @RequestMapping("/cargo")
 public class CargoController {
+	
+	@Autowired
+	private Configuration configuration;
 
 	@GetMapping
-	public String getName() {
-		return "Hello World";
+	public Cargo getName() {
+		return new Cargo(configuration.getPriceOfDelivery(), configuration.getPriceOfShipping());
 	}
 }
